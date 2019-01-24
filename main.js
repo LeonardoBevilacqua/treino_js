fdd2c6667d65f4efcbcb1d5c731e5beaad51c832
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-01-24 14:09:12 -0200

    janelas arrastáveis (teste)

--- main.js
+++ main.js
@@ -38,3 +38,50 @@ function toggle_panel(div) {
 
    }
 }
+
+/* ========================= arrastar janelas ========================= */
+//dragElement(document.getElementById("mydiv"));
+
+for (let i = 0; i < document.getElementsByClassName("janela").length; i++) {
+   const element = document.getElementsByClassName("janela")[i];
+   console.log(element);
+   dragElement(element);
+   
+}
+
+function dragElement(element) {
+   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+   if (document.getElementById(element.id + "header")) {
+      document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+   }   
+   
+   function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+   
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+
+      document.onmousemove = elementDrag;
+   }
+
+   function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";     
+   }
+
+   function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+   }
+
+}
