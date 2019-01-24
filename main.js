2f40ff8872f36a7b62e75a15dcf58154d56660a6
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-01-24 11:06:55 -0200

    Painel deslizavel

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* ========================= Lista ========================= */
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
@@ -14,4 +15,26 @@ for (let i = 0; i < coll.length; i++) {
          icon.className = "fa-li fa fa-minus-square";
       }
    });
+}
+
+/* ========================= Painel ========================= */
+function toggle_panel(div) {
+   div.classList.toggle("change");
+
+   var leftPanel = document.getElementById("left-panel");
+
+   if (div.classList.contains('change')) {      
+      document.getElementById("left-panel").style.width = null;
+      document.getElementById("left-panel").style.marginLeft = null;      
+
+      document.getElementById("left-panel").classList.toggle("col-3");
+      document.getElementById("left-panel").classList.toggle("col-s-5");
+   } else {
+      document.getElementById("left-panel").classList.toggle("col-3");
+      document.getElementById("left-panel").classList.toggle("col-s-5");
+
+      document.getElementById("left-panel").style.width = "0";
+      document.getElementById("left-panel").style.marginLeft = "-25%";      
+
+   }
 }
