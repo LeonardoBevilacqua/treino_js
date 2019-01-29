81964e51c25c664b325e42088592e35a876adedd
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-01-29 11:21:54 -0200

    consulta, paginação, server simples

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,4 +1,8 @@
 var acao = null;
+var consulta_ativa = false;
+var pos = 0;
+var search_query = null;
+var url_base = "https://teste-leonardoabevilacqua148955.codeanyapp.com/";
 var acoes = ["incluir",
    "modificar",
    "excluir",
@@ -11,30 +15,55 @@ var acoes = ["incluir",
    "cancelar",
    "sair"];
 
+var dados = [];
+
 function carrega_dados(data) {
+   var url = url_base + data + ".php"
+   for (let i = 0; i < campos.length; i++) {
+      const element = campos[i];
+      if (!element.disabled) {
+         if (element.value != "") {
+            if (!search_query) {
+               search_query = "?" + element.id + "=" + element.value;
+            } else {
+               search_query += "&" + element.id + "=" + element.value;
+            }
+         }
+      }
+   }
+   if (search_query) {
+      url += search_query;
+      search_query = null;
+   }
+
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-         var json = JSON.parse(this.responseText);
-         var array = Object.keys(json[1]).map(i => json[1][i]);
-         //console.log(array);
+      if (this.readyState === 4) {
+         if (this.status === 200) {
+            dados = JSON.parse(this.responseText);
 
-         for (let i = 0; i < array.length; i++) {
-            if (!campos[i]) {
-               console.warn("Campo faltando!");
-               continue;
-            }
-            var element = array[i];
-            campos[i].value = element;
+            exibe_dados(Object.values(dados[pos]));
 
+            consulta_ativa = true;
+            altera_estado_menu();
+            altera_estado_campos();
+         } else {
+            acao_popup(true, "Erro", "Falha ao carrgar os dados!\nCodigo: " + this.status);
+            console.error(this.statusText);
+            altera_estado_menu();
+            altera_estado_campos();
          }
       }
    };
-   xhttp.open("GET", "json/" + data + ".json", true);
+   xhttp.open("GET", url, true);
    xhttp.send();
+}
 
-   altera_estado_menu();
-   altera_estado_campos();
+function exibe_dados(data) {
+   for (let y = 0; y < campos.length; y++) {
+      const field = campos[y];
+      field.value = data[y];
+   }
 }
 
 function altera_estado_menu() {
@@ -67,10 +96,38 @@ function acao_cancelar() {
    altera_estado_campos();
 }
 
-function acao_confirmar() {
+function acao_confirmar(button) {
    if (acao == acoes[3]) {
-      carrega_dados("clientes");
+      carrega_dados(button.id.split("-")[0]);
    }
+}
+
+function acao_paginar(funcao) {
+   if (!consulta_ativa) {
+      acao_popup(true, "Alerta", "realize a consulta primeiro!");
+      return;
+   }
+
+   switch (funcao) {
+      case acoes[4]:
+         if (pos > 0) {
+            pos--;
+         } else {
+            acao_popup(true, "Alerta", "Sem dados nessa direção");
+            return;
+         }
+         break;
+      case acoes[5]:
+         if (pos < dados.length - 1) {
+            pos++;
+         } else {
+            acao_popup(true, "Alerta", "Sem dados nessa direção");
+            return;
+         }
+      default:
+         break;
+   }
+   exibe_dados(Object.values(dados[pos]));
 }
 
 function acao_popup(acao, title, body) {
@@ -89,8 +146,8 @@ function acao_sair() {
    janela_atual.parentNode.removeChild(janela_atual);
 }
 
-/* teste */
-document.getElementById("link-pedido").click();
+/* teste 
+document.getElementById("link-clientes").click();
 document.getElementById("toggle-panel").click();
-
+*/
 
